a6f78f9cbc05549125be6078b67247a3c0b4896a
john <johntucker0686@gmail.com>
2020-10-13 11:00:18 -0700

    add start/pause

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let squares = Array.from(document.querySelectorAll(".grid div"));
-  const ScoreDisplay = document.querySelector("#score");
-  const StartBtn = document.querySelector("#start-buton");
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
   const width = 10;
   let nextRandom = 0;
+  let timerId;
 
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -66,8 +67,6 @@ document.addEventListener("DOMContentLoaded", () => {
       squares[currentPosition + index].classList.remove("tetromino");
     });
   }
-
-  timerId = setInterval(moveDown, 1000);
 
   function control(e) {
     if (e.keyCode === 37) {
@@ -172,4 +171,16 @@ document.addEventListener("DOMContentLoaded", () => {
       displaySquares[displayIndex + index].classList.add("tetromino");
     });
   }
+
+  startBtn.addEventListener("click", () => {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = null;
+    } else {
+      draw();
+      timerId = setInterval(moveDown, 1000);
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+      displayShape();
+    }
+  });
 });
